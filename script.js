3f32bdd6496e5fe7a81df8932382d842083ddce8
Gautam-2002 <gautam2sethi@gmail.com>
2022-09-02 15:33:17 +0530

    edge cases left

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-var result_str="";
-var result=document.getElementsByClassName("result");
-var reset=document.getElementsByClassName("clear");
-var sign=document.getElementsByClassName("sign");
-var mod=document.getElementsByClassName("mod");
-var divide=document.getElementsByClassName("divide");
-var seven=document.getElementsByClassName("seven");
-var eight=document.getElementsByClassName("eight");
-var nine=document.getElementsByClassName("nine");
-var multiply=document.getElementsByClassName("multiply");
-var four=document.getElementsByClassName("four");
-var five=document.getElementsByClassName("five");
-var six=document.getElementsByClassName("six");
-var subtract=document.getElementsByClassName("subtract");
-var three=document.getElementsByClassName("three");
-var two=document.getElementsByClassName("two");
-var one=document.getElementsByClassName("one");
-var add=document.getElementsByClassName("add");
-var zero=document.getElementsByClassName("zero");
-var point=document.getElementsByClassName("point");
-var equl=document.getElementsByClassName("equl");
-reset.addEventListener('click',function(){
-    result_str="";
-});
-seven.addEventListener('click',function(){
-    result_str+="7";
-    result.innerText=result_str;
-    console.console.log("seven");
-});
+function isOperator(str) {
+    return str == "+" || str == "-" || str == "*" || str == "/";
+}
+function display(){
+    let str = (this.innerText) ;
+    if(isOperator(str)){
+        panel.innerText = "";
+        ans += num;
+        num="";
+        ans += str;
+    }
+    else if(str == "+/-"){
+        num*=-1;
+        panel.innerText = num;
+    }
+    else if(str == "AC"){
+    panel.innerText = "";
+    ans ="";
+    num="";
+    }
+    else if(str == "%"){
+    panel.innerText = str;
+    ans += "*100";
+    }
+    else{
+    num +=str;
+    panel.innerText = num;
+    }
+}
+function output(){
+    ans += num;
+    console.log("num = "+num)
+    console.log("ans = "+ans)
+    var res = eval(ans);
+    panel.innerText = res;
+    ans = "";
+    num = res;
+}
+let ans = "";
+let num ="";
+var text = document.querySelectorAll(".level3");
+var panel = document.querySelector('.result');
+text.forEach((Node) => {Node.addEventListener('click',display)})
+document.querySelector('#equl').removeEventListener('click',display);
+document.querySelector('#equl').addEventListener('click',output);
